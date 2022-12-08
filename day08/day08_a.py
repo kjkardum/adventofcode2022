b664432a95737e54acba02bff1fd241a12646694
kjkardum <kjkardum@gmail.com>
2022-12-08 10:40:05 +0100

    Fancy day8

--- day08/day08_a.py
+++ day08/day08_a.py
@@ -1,50 +1,23 @@
 if __name__ == "__main__":
     data = [list(map(int, line.strip())) for line in open("input.txt")]
     n = len(data)
-    max_left, max_top, max_right, max_bottom = [], [], [], []
+    max_left, max_top, max_right, max_bottom = ([[0] * n for _ in range(n)] for _ in range(4))
 
     for i in range(n):
-        max_top.append([])
-        max_left.append([])
         for j in range(n):
-            if i == 0 and j == 0:
-                max_left[-1].append(data[i][j])
-                max_top[-1].append(data[i][j])
-            elif i == 0:
-                max_left[-1].append(max(data[i][j], max_left[-1][-1]))
-                max_top[-1].append(data[i][j])
-            elif j == 0:
-                max_left[-1].append(data[i][j])
-                max_top[-1].append(max(data[i][j], max_top[-2][j]))
-            else:
-                max_left[-1].append(max(data[i][j], max_left[-1][-1]))
-                max_top[-1].append(max(data[i][j], max_top[-2][j]))
-    for i in range(n-1, -1, -1):
-        max_bottom.append([])
-        max_right.append([])
-        for j in range(n-1, -1, -1):
-            if i == n-1 and j == n-1:
-                max_bottom[-1].append(data[i][j])
-                max_right[-1].append(data[i][j])
-            elif i == n-1:
-                max_bottom[-1].append(data[i][j])
-                max_right[-1].append(max(data[i][j], max_right[-1][-1]))
-            elif j == n-1:
-                max_bottom[-1].append(max(data[i][j], max_bottom[-2][j]))
-                max_right[-1].append(data[i][j])
-            else:
-                max_bottom[-1].append(max(data[i][j], max_bottom[-2][j]))
-                max_right[-1].append(max(data[i][j], max_right[-1][-1]))
-        max_bottom[-1].reverse()
-        max_right[-1].reverse()
-    max_bottom.reverse()
-    max_right.reverse()
-    
+            re_i, re_j = n - 1 - i, n - 1 - j
+            max_top[i][j] = data[i][j] if i == 0 else max(data[i][j], max_top[i - 1][j])
+            max_left[i][j] = data[i][j] if j == 0 else max(data[i][j], max_left[i][j - 1])
+            max_bottom[re_i][re_j] = data[re_i][re_j] if re_i == n - 1 else max(data[re_i][re_j], max_bottom[re_i + 1][re_j])
+            max_right[re_i][re_j] = data[re_i][re_j] if re_j == n - 1 else max(data[re_i][re_j], max_right[re_i][re_j + 1])
     peaks_number = 0
     for i in range(n):
         for j in range(n):
-            if i==0 or j==0 or i==n-1 or j==n-1:
-                peaks_number += 1
-            elif data[i][j] > max_left[i][j-1] or data[i][j] > max_top[i-1][j] or data[i][j] > max_right[i][j+1] or data[i][j] > max_bottom[i+1][j]:
-                peaks_number += 1
+            peaks_number += \
+                i == 0 or j == 0 or i == n - 1 or j == n - 1 or \
+                data[i][j] > max_left[i][j - 1] or \
+                data[i][j] > max_top[i - 1][j] or \
+                data[i][j] > max_right[i][j + 1] or \
+                data[i][j] > max_bottom[i + 1][j]
     print(peaks_number)
+    
--- day08/day08_b.py
+++ day08/day08_b.py
@@ -1,49 +1,15 @@
 if __name__ == "__main__":
     data = [list(map(int, line.strip())) for line in open("input.txt")]
     n = len(data)
-    visible_left, visible_right, visible_top, visible_bottom = [], [], [], []
+    visible_left, visible_right, visible_top, visible_bottom = ([[[0] * 10 for _ in range(n)] for _ in range(n)] for _ in range(4))
     for i in range(n):
-        visible_left.append([])
-        visible_top.append([])
         for j in range(n):
-            visible_left[-1].append([])
-            visible_top[-1].append([])
-            for k in range(10):
-                if i == 0 and j == 0:
-                    visible_left[-1][-1].append(0)
-                    visible_top[-1][-1].append(0)
-                elif i == 0:
-                    visible_left[-1][-1].append(visible_left[-1][-2][k] + 1 if data[i][j-1] < k else 1)
-                    visible_top[-1][-1].append(0)
-                elif j == 0:
-                    visible_left[-1][-1].append(0)
-                    visible_top[-1][-1].append(visible_top[-2][j][k] + 1 if data[i-1][j] < k else 1)
-                else:
-                    visible_left[-1][-1].append(visible_left[-1][-2][k] + 1 if data[i][j-1] < k else 1)
-                    visible_top[-1][-1].append(visible_top[-2][j][k] + 1 if data[i-1][j] < k else 1)
-    for i in range(n - 1, -1, -1):
-        visible_right.append([])
-        visible_bottom.append([])
-        for j in range(n - 1, -1, -1):
-            visible_right[-1].append([])
-            visible_bottom[-1].append([])
-            for k in range(10):
-                if i == n-1 and j == n-1:
-                    visible_right[-1][-1].append(0)
-                    visible_bottom[-1][-1].append(0)
-                elif i == n-1:
-                    visible_right[-1][-1].append(visible_right[-1][-2][k] + 1 if data[i][j + 1] < k else 1)
-                    visible_bottom[-1][-1].append(0)
-                elif j == n-1:
-                    visible_right[-1][-1].append(0)
-                    visible_bottom[-1][-1].append(visible_bottom[-2][j][k] + 1 if data[i + 1][j] < k else 1)
-                else:
-                    visible_right[-1][-1].append(visible_right[-1][-2][k] + 1 if data[i][j + 1] < k else 1)
-                    visible_bottom[-1][-1].append(visible_bottom[-2][j][k] + 1 if data[i + 1][j] < k else 1)
-        visible_right[-1].reverse()
-        visible_bottom[-1].reverse()
-    visible_right.reverse()
-    visible_bottom.reverse()
+            re_i, re_j = n - 1 - i, n - 1 - j
+            for h in range(10):
+                visible_left[i][j][h] = j!=0 and (visible_left[i][j - 1][h] + 1 if data[i][j - 1] < h else 1)
+                visible_top[i][j][h] = i!=0 and (visible_top[i - 1][j][h] + 1 if data[i - 1][j] < h else 1)
+                visible_right[re_i][re_j][h] = re_j!=n-1 and (visible_right[re_i][re_j + 1][h] + 1 if data[re_i][re_j + 1] < h else 1)
+                visible_bottom[re_i][re_j][h] = re_i!=n-1 and (visible_bottom[re_i + 1][re_j][h] + 1 if data[re_i + 1][re_j] < h else 1)
 
     visible_top = [[visible_top[i][j][data[i][j]] for j in range(n)] for i in range(n)]
     visible_left = [[visible_left[i][j][data[i][j]] for j in range(n)] for i in range(n)]

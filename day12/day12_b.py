0265b8bc7a2560e36c353bc85c7b2cc3c6846489
kjkardum <kjkardum@gmail.com>
2022-12-12 13:39:55 +0100

    day12 neater solution

--- day12/day12_b.py
+++ day12/day12_b.py
@@ -1,18 +1,28 @@
-if __name__ == "__main__":
-    map = open("input.txt").read().splitlines()
-    start = next((i, j) for i in range(len(map)) for j in range(len(map[0])) if map[i][j] == "E")
+def find_path(M, start, end, next_condition):
     queue, visited = [(*start, 0)], []
     while queue:
         x, y, length = queue.pop(0)
         if (x, y) in visited:
             continue
-        if map[x][y] == "a":
-            print(length)
-            break
+        if M[x][y] == end:
+            return length
         visited.append((x, y))
         for i, j in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
-            if 0 <= i < len(map) and 0 <= j < len(map[0]):
-                order = map[i][j] != "S" and ord(map[i][j])
-                my_order = ord(map[x][y] if map[x][y] != "E" else "z") - 1
-                if order >= my_order:
+            if 0 <= i < len(M) and 0 <= j < len(M[0]):
+                if next_condition(M, x, y, i, j):
                     queue.append((i, j, length + 1))
+
+if __name__ == "__main__":
+    M = open("input.txt").read().splitlines()
+    start_S = next(((i, row.index("S")) for i, row in enumerate(M) if "S" in row))
+    start_E = next(((i, row.index("E")) for i, row in enumerate(M) if "E" in row))
+    def part1(M, from_x, from_y, to_x, to_y):
+        my_order = M[from_x][from_y] if M[from_x][from_y] != "S" else "z"
+        order = M[to_x][to_y] if M[to_x][to_y] != "E" else "z"
+        return ord(order) <= ord(my_order) + 1
+    def part2(M, from_x, from_y, to_x, to_y):
+        my_order = M[from_x][from_y] if M[from_x][from_y] != "E" else "z"
+        order = M[to_x][to_y] if M[to_x][to_y] != "S" else "z"
+        return ord(order) + 1 >= ord(my_order)
+    print(find_path(M, start_S, "E", part1))
+    print(find_path(M, start_E, "a", part2))
